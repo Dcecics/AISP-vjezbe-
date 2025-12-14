e2dc4e6f3be138f504de672fb9900c920a2cbe96
Dcecics <124448621+Dcecics@users.noreply.github.com>
2025-12-14 17:04:17 +0100

    Vjezba 6

--- Source.c
+++ Source.c
@@ -3,201 +3,135 @@
 #include <stdlib.h>
 #include <time.h>
 #include <string.h>
-
+#define MAX 100
+#define MIN 10
 struct node;
 typedef struct node* Position;
 struct node {
 	int element;
 	Position next;
 };
-int readFromFile(Position);
-int presjek(Position, Position, Position);
-int printList(Position);
-int unija(Position, Position, Position);
+
+int PushStack(Position, int);
+int PushQueue(Position, int);
+int Pop(Position);
+int getRandom();
+int Print(Position);
 int deletAll(Position);
+
 int main() {
 
-	struct node head1;
-	struct node head2;
-	struct node headPresjek;
-	struct node headUnija;
-	int result = 0;
-	head1.next = NULL;
-	head2.next = NULL;
-	headPresjek.next = NULL;
-	headUnija.next = NULL;
-	result = readFromFile(&head1);
-	if (result == 1) {
-		printf("Error datoteka nije otvorena!\n");
-		return 0;
-	}
-	else if (result == -1) {
-		printf("Error memorija nije alocirana!\n");
-		deletAll(&head1);
-		return 0;
-	}
-	else {
-		printf("Sve je proslo u redu!\n");
-	}
-	printf("Lista1:\t");
-	printList(head1.next);
-	result = readFromFile(&head2);
-	if (result == 1) {
-		printf("Error memorija nije alocirana!\n");
-		deletAll(&head1);
-		return 0;
-	}
-	else if (result == -1) {
-		printf("Error memorija nije alocirana!\n");
-		deletAll(&head1);
-		deletAll(&head2);
-		return 0;
-	}
-	else {
-		printf("Sve je proslo u redu!\n");
-	}
-	printf("Lista2:\t");
-	printList(head2.next);
-	result = presjek(head1.next, head2.next, &headPresjek);
-	if (result == -2) {
-		printf("Error memorija nije alocirana!\n");
-		deletAll(&head1);
-		deletAll(&head1);
-		deletAll(&headPresjek);
-		return 0;
-	}
-	else {
-		printf("Sve je proslo u redu!\n");
-	}
-	printf("Presjek:");
-	printList(headPresjek.next);
-	result = unija(head1.next, head2.next, &headUnija);
-	if (result == -2) {
-		printf("Error memorija nije alocirana!\n");
-		deletAll(&head1);
-		deletAll(&head1);
-		deletAll(&headUnija);
-		return 0;
-	}
-	else {
-		printf("Sve je proslo u redu!\n");
-	}
-	printf("Unija:\t");
-	printList(headUnija.next);
-	deletAll(&head1);
-	deletAll(&head2);
-	deletAll(&headPresjek);
-	deletAll(&headUnija);
-	printList(headPresjek.next);
-	printList(headUnija.next);
-	printList(head1.next);
-	printList(head2.next);
-	return 0;
-}
-int readFromFile(Position P) {
-	FILE* fp = NULL;
-	char fileName[128] = "\0";
-	Position q = NULL;
-	printf("Upisite ime datoteke:\n");
-	scanf(" %s", fileName);
-	fp = fopen(fileName, "r");
-	if (fp == NULL) {
-		printf("Datoteka nije pronadena!!\n");
-		return 1;
-	}
-	while (!feof(fp)) {
-		q = (Position)malloc(sizeof(struct node));
-		if (q == NULL) {
-			printf("Alokacija nije uspijela!!\n");
-			fclose(fp);
-			return -1;
+	struct node headStack;
+	struct node headQueue;
+	int resualt = 0, choice = 0;
+	headQueue.next = NULL;
+	headStack.next = NULL;
+	printf("Unesite sto zelite raditi:\n1.Push na stog\n2.Pop iz stog\n3.Ispisi stog\n4.Ispisi red\n5.Push na red\n6.Pop iz reda\n");
+	do {
+		printf("Unesi 1,2,3,4,5 ili 6\n");
+		scanf(" %d", &choice);
+		if (choice < 1 || choice > 6) {
+			printf(" Netocan unos!!!\n");
 		}
-
-		while (P->next != NULL) {
-			P = P->next;
-		}
-		fscanf(fp, "%d", &q->element);
-		q->next = P->next;
-		P->next = q;
-	}
-	fclose(fp);
-	return 0;
-}
-int presjek(Position L1, Position L2, Position P) {
-	Position q = NULL;
-	while (L1 != NULL && L2 != NULL)
-	{
-		if (L1->element == L2->element) {
-			q = (Position)malloc(sizeof(struct node));
-			if (q == NULL) {
-				printf("Alokacija nije uspijela!!\n");
-				return -1;
+		else
+		{
+			switch (choice) {
+			case 1:
+				resualt = PushStack(&headStack, getRandom());
+				if (resualt == -1) {
+					printf(" Greska.\n");
+				}
+				else {
+					printf(" Sve je proslo u redu.\n");
+				}
+				break;
+			case 2:
+				resualt = Pop(&headStack);
+				break;
+			case 3:
+				printf("Stog je:");
+				Print(headStack.next);
+				break;
+			case 4:
+				printf("Red je:");
+				Print(headQueue.next);
+				break;
+			case 5:
+				resualt = PushQueue(&headQueue, getRandom());
+				if (resualt == -1) {
+					printf(" Greska.\n");
+				}
+				else {
+					printf(" Sve je proslo u redu.\n");
+				}
+				break;
+			case 6:
+				resualt = Pop(&headQueue);
+				break;
 			}
-			q->element = L1->element;   // Uzimamo element iz L1 (ili L2, jer su jednaki)
-			q->next = P->next;          // Novi èvor pokazuje na trenutni presjek
-			P->next = q;                // Glava presjeka pokazuje na novi èvor
-			P = P->next;
-			L1 = L1->next;
-			L2 = L2->next;              //sve tri liste se pomicu na sljedeci
 		}
-		else if (L1->element > L2->element) {
-			L2 = L2->next;              //l2 je manji i ide dalje(liste su sortirane)
-		}
-		else if (L1->element < L2->element) {
-			L1 = L1->next;              //isto kao i za l2
-		}
+		printf("Ako zelite stati unesite 1, ako zelite nastaviti unesite 0!\n");
+		scanf(" %d", &choice);
+	} while (choice != 1);
+	deletAll(&headStack);
+	deletAll(&headQueue);
+	return 0;
+}
+int getRandom() {
+	int value = 0;
+	//srand((unsigned)time(NULL));
+	value = rand() % ((MAX - MIN + 1) + MIN);
+	return value;
+}
+int PushStack(Position P, int value) {
+	Position temp;
+	temp = (Position)malloc(sizeof(struct node));
+	if (temp == NULL) {
+		printf("Alokacija nije uspijela!!");
+		return -1;
+	}
+	temp->element = value;
+	temp->next = P->next;
+	P->next = temp;
+	return 0;
+}
+int Pop(Position P) {
+	Position temp = NULL;
+	if (P->next != NULL) {
+		temp = P->next;
+		P->next = temp->next;
+		printf("Izbrisana vrijednost je %d\n", temp->element);
+		free(temp);
 	}
 	return 0;
 }
-int printList(Position P) {
+int Print(Position P) {
 	if (P == NULL) {
 		printf("Prazna lista\n");
 		return 0;
 	}
 	while (P != NULL) {
-		printf(" %d\t", P->element);
+		printf(" %d ", P->element);
 		P = P->next;
 	}
 	printf("\n");
 	return 0;
 }
-int unija(Position L1, Position L2, Position U) {
-	Position q = NULL;
-	while (L1 != NULL || L2 != NULL)
-	{
-		q = (Position)malloc(sizeof(struct node));
-		if (q == NULL) {
-			printf("Alokacija nije uspijela!!\n");
-			return -1;
-		}
-		if (L1 == NULL) {             //lista l1 je prazna svi preostali elementi l2 se dodaju u uniju
-			q->element = L2->element;
-			L2 = L2->next;
-		}
-		else if (L2 == NULL) {
-			q->element = L1->element;
-			L1 = L1->next;
-		}
-		else
-		{
-			if (L1->element > L2->element) {    //ako su l1 i l2 razlicite manji element se dodaje u uniju
-				q->element = L2->element;
-				L2 = L2->next;
-			}
-			else if (L1->element < L2->element) {
-				q->element = L1->element;
-				L1 = L1->next;
-			}
-			else {                     //elementi su jednaki, dodaje se samo jedan od njih i oba pokazivaca se pomicu
-				q->element = L1->element;
-				L1 = L1->next;
-				L2 = L2->next;
-			}
-		}
-		q->next = U->next;
-		U->next = q;
-		U = U->next;       //pomicemo se na sljedeci da se novi element doda iza ovog kojeg smo vec upisali
+int PushQueue(Position  P, int value) {
+	static Position last = NULL;
+	Position temp = NULL;
+	if (last == NULL) {
+		last = P;
 	}
+	temp = (Position)malloc(sizeof(struct node));
+	if (temp == NULL) {
+		printf("Alokacija nije uspijela!!");
+		return -1;
+	}
+	temp->element = value;
+	temp->next = last->next;
+	last->next = temp;
+	last = last->next;
 	return 0;
 }
 int deletAll(Position P) {
