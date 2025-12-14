ee08850993962dc6369eaf30a4f9f19a72f90929
Dcecics <124448621+Dcecics@users.noreply.github.com>
2025-12-14 17:05:43 +0100

    Vjezbe 9 i 10

--- Source.c
+++ Source.c
@@ -1,148 +1,300 @@
 #define _CRT_SECURE_NO_WARNINGS
-#include <stdio.h>
-#include <stdio.h>
-#include <stdlib.h>
-#include <time.h>
-#include <string.h>
+#include<stdio.h>
+#include<stdlib.h>
+#include<string.h>
+#include<time.h>
+#include<windows.h>
+#define N 10000
 
+void ExchangeSort(int*);
+void SelectionSort(int*);
+void InsertionSort(int*);
+void BubbleSort(int*);
+void QuickSort(int*, int, int);
+void MergeSort(int*, int, int);
+void ShellSort(int*, int);
+void CopyArray(int*, int*);
+void RandomGenerate(int*);
+int partition(int*, int, int);
+void swap(int*, int, int);
+int FindMax(int*, int);
 
-struct node;
-typedef struct node* Position;
-struct node {
-    int element;
-    Position next;
-
-
-};
-int PushStack(Position, int);
-int Pop(Position);
-int calculate(Position);
-int Print(Position);
-void deleteAll(Position);
 int main()
 {
+	clock_t start, stop;
+	int array[N] = { 0 };
+	int copy[N] = { 0 };
 
-    struct node Head;
-    Head.next = NULL;
-    FILE* fp;
+	RandomGenerate(array);
+	CopyArray(array, copy);
 
-    int operand1, operand2, rez = 0;
-    char ch = '\0';
-    int num = 0;
+	start = clock();
+	ExchangeSort(array);
+	stop = clock();
+	printf("Approx seconds, tenths, hundredths and milliseconds: %.3f\n", ((double)(stop - start) / CLOCKS_PER_SEC));
 
-    fp = fopen("postfiks.txt", "r");
-    if (fp == NULL) {
-        printf("Datoteku je nemoguce otvoriti!!!");
-        return -1;
-    }
+	CopyArray(copy, array);
 
+	start = clock();
+	SelectionSort(array);
+	stop = clock();
+	printf("Approx seconds, tenths, hundredths and milliseconds: %.3f\n", ((double)(stop - start) / CLOCKS_PER_SEC));
 
-    while (!feof(fp)) {
-        ch = fgetc(fp);
-        if (ch >= '0' && ch <= '9') {
-            ungetc(ch, fp);
-            fscanf(fp, "%d", &num);
-            PushStack(&Head, num);
-        }
-        else if (ch != 32 && ch != '\n' && ch != EOF) {
-            operand2 = Pop(&Head);
-            operand1 = Pop(&Head);
-            switch (ch) {
+	CopyArray(copy, array);
 
-            case '+':
+	start = clock();
+	InsertionSort(array);
+	stop = clock();
+	printf("Approx seconds, tenths, hundredths and milliseconds: %.3f\n", ((double)(stop - start) / CLOCKS_PER_SEC));
 
-                rez = operand1 + operand2;
-                break;
-            case '-':
+	CopyArray(copy, array);
 
-                rez = operand1 - operand2;
-                break;
-            case '/':
+	start = clock();
+	BubbleSort(array);
+	stop = clock();
+	printf("Approx seconds, tenths, hundredths and milliseconds: %.3f\n", ((double)(stop - start) / CLOCKS_PER_SEC));
 
-                if (operand2 != 0) {
-                    rez = operand1 / operand2;
-                    break;
-                }
-                else {
-                    printf("Error, cannot be divided with 0\n");
-                    break;
-                }
-            case '*':
+	CopyArray(copy, array);
 
-                rez = operand1 * operand2;
-                break;
+	start = clock();
+	ShellSort(array, N);
+	stop = clock();
+	printf("Approx seconds, tenths, hundredths and milliseconds: %.3f\n", ((double)(stop - start) / CLOCKS_PER_SEC));
 
-            default:
-                printf("Invalid operator: %c\n", ch);
-                fclose(fp);
-                deleteAll(&Head);
-                return 1;
-                break;
+	CopyArray(copy, array);
 
+	start = clock();
+	QuickSort(array, 0, N - 1);
+	stop = clock();
+	printf("Approx seconds, tenths, hundredths and milliseconds: %.3f\n", ((double)(stop - start) / CLOCKS_PER_SEC));
 
-            }
-            PushStack(&Head, rez);
+	CopyArray(copy, array);
 
-        }
-    }
-    printf("\nFinal result of postfix is: %d\n", rez);
-    Print(Head.next);
-    deleteAll(&Head);
-    fclose(fp);
+	start = clock();
+	MergeSort(array, 0, N - 1);
+	stop = clock();
+	printf("Approx seconds, tenths, hundredths and milliseconds: %.3f\n", ((double)(stop - start) / CLOCKS_PER_SEC));
 
+	return 0;
+}
 
-
-    return 0;
+void CopyArray(int* source, int* destination)
+{
+	int i = 0;
+	for (i = 0; i < N; i++)
+	{
+		destination[i] = source[i];
+	}
 
 }
-int Print(Position P) {
-    if (P == NULL) {
-        printf("Prazna lista\n");
-        return 0;
-    }
-    while (P != NULL) {
-        printf(" %d\n", P->element);
-        P = P->next;
-    }
-    printf("\n");
-    return 0;
+
+void RandomGenerate(int* array)
+{
+	int i = 0;
+	time_t t;
+	srand((unsigned)time(&t));
+	for (i = 0; i < N; i++)
+	{
+		array[i] = rand();
+	}
 }
-int PushStack(Position P, int value) {
-    Position temp;
 
-
-    temp = (Position)malloc(sizeof(struct node));
-    if (temp == NULL) {
-        printf("Memory is not allocated!!!");
-        return -1;
-    }
-    temp->element = value;   // Sprema broj u novi èvor
-    temp->next = P->next;    // Novi èvor pokazuje na prvi element stoga
-    P->next = temp;          // Novi èvor postaje vrh stoga
-
-    Print(P->next);          // Ispisuje trenutno stanje stoga
-
-    return 0;
+void ExchangeSort(int* array)
+{
+	int i, j, temp;
+	for (i = 0; i < N - 1; i++)
+	{
+		for (j = i + 1; j < N; j++)
+		{
+			if (array[i] > array[j])
+			{
+				swap(array, j, i);
+			}
+		}
+	}
 }
-int Pop(Position P) {
-    Position temp = NULL;
-    int value = 0;
-    if (P->next != NULL) {
-        temp = P->next;           // Pokazivaè na vrh stoga
-        P->next = temp->next;     // Pomièe vrh na sljedeæi element
-        value = temp->element;    // Sprema vrijednost vrha
-        free(temp);               // Briše stari vrh iz memorije
-    } 
-    return value;                  // Vraæa vrijednost vrha
+
+void SelectionSort(int* array)
+{
+	int i, max;
+	for (i = 0; i < N; i++)
+	{
+		max = FindMax(array, i);
+		swap(array, max, (N - 1 - i));
+	}
 }
-void deleteAll(Position P) {
-    Position temp = NULL;
-    while (P->next != NULL)
-    {
-        temp = P->next;
-        P->next = temp->next;
-        free(temp);
 
-    }
+void BubbleSort(int* array)
+{
+	int i, j;
+	for (i = 0; i < N - 1; i++)
+	{
+		for (j = 0 + 1; j < N; j++)
+		{
+			if (array[i] > array[j])
+			{
+				swap(array, j, i);
+			}
+		}
+	}
+}
 
+int FindMax(int* array, int high)
+{
+	int i, index;
+	index = high;
+	for (i = high; i < N; i++)
+	{
+		if (array[i] > array[index])
+			index = i;
+	}
+	return index;
+}
+
+void swap(int* array, int p1, int p2)
+{
+	int temp;
+	temp = array[p2];
+	array[p2] = array[p1];
+	array[p1] = temp;
+}
+
+void InsertionSort(int* array)
+{
+	int i, key, j;
+	for (i = 1; i < N; i++)
+	{
+		key = array[i];
+		j = i - 1;
+
+		while (j >= 0 && array[j] > key)
+		{
+			array[j + 1] = array[j];
+			j = j - 1;
+		}
+		array[j + 1] = key;
+	}
+}
+
+int partition(int array[], int low, int high) {
+
+	// Initialize pivot to be the first element
+	int p = array[low];
+	int i = low;
+	int j = high;
+
+	while (i < j) {
+
+		// Find the first element greater than
+		// the pivot (from starting)
+		while (array[i] <= p && i <= high - 1) {
+			i++;
+		}
+
+		// Find the first element smaller than
+		// the pivot (from last)
+		while (array[j] > p && j >= low + 1) {
+			j--;
+		}
+		if (i < j) {
+			swap(array, i, j);
+		}
+	}
+	swap(array, low, j);
+	return j;
+}
+
+
+void QuickSort(int array[], int low, int high) {
+	if (low < high) {
+
+		// call partition function to find Partition Index
+		int pi = partition(array, low, high);
+
+		// Recursively call quickSort() for left and right
+		// half based on Partition Index
+		QuickSort(array, low, pi - 1);
+		QuickSort(array, pi + 1, high);
+	}
+}
+
+void ShellSort(int a[], int b)
+{
+	/* Rearranging the elements of array at n/2, n/4, ..., 1 intervals */
+	for (int interval = b / 2; interval > 0; interval /= 2)
+	{
+		for (int i = interval; i < b; i += 1)
+		{
+			/* store a[i] to the tempvar variable and make the ith position empty */
+			int tempvar = a[i];
+			int j;
+			for (j = i; j >= interval && a[j - interval] > tempvar; j -= interval)
+				a[j] = a[j - interval];
+
+			// put tempvar which is a[i] in its correct position  
+			a[j] = tempvar;
+		}
+	}
+	return 0;
+}
+
+void Merge(int arr[], int l, int m, int r)
+{
+	int i, j, k;
+	int n1 = m - l + 1;
+	int n2 = r - m;
+
+	// Create temp arrays
+	int L[(N / 2) + 1], R[N / 2];
+
+	// Copy data to temp arrays L[] and R[]
+	for (i = 0; i < n1; i++)
+		L[i] = arr[l + i];
+	for (j = 0; j < n2; j++)
+		R[j] = arr[m + 1 + j];
+
+	// Merge the temp arrays back into arr[l..r
+	i = 0;
+	j = 0;
+	k = l;
+	while (i < n1 && j < n2) {
+		if (L[i] <= R[j]) {
+			arr[k] = L[i];
+			i++;
+		}
+		else {
+			arr[k] = R[j];
+			j++;
+		}
+		k++;
+	}
+
+	// Copy the remaining elements of L[],
+	// if there are any
+	while (i < n1) {
+		arr[k] = L[i];
+		i++;
+		k++;
+	}
+
+	// Copy the remaining elements of R[],
+	// if there are any
+	while (j < n2) {
+		arr[k] = R[j];
+		j++;
+		k++;
+	}
+}
+
+void MergeSort(int arr[], int l, int r)
+{
+	if (l < r) {
+		int m = l + (r - l) / 2;
+
+		// Sort first and second halves
+		MergeSort(arr, l, m);
+		MergeSort(arr, m + 1, r);
+
+		Merge(arr, l, m, r);
+	}
 }
