b3abf3a523b3c7285f3e5b1629fa2813cff0daad
Dcecics <124448621+Dcecics@users.noreply.github.com>
2025-12-14 17:03:01 +0100

    Vjezba 4

--- Source.c
+++ Source.c
@@ -1,333 +1,239 @@
 #define _CRT_SECURE_NO_WARNINGS
 #include <stdio.h>
 #include <stdlib.h>
+#include <time.h>
 #include <string.h>
-#define NAME_LEN 50
 
-typedef struct osoba Osoba;
-typedef struct osoba* Position;
-
-struct osoba {
-	char firstname[NAME_LEN], lastname[NAME_LEN];
-	int birthYear;
-	Position Next;
+struct node;
+typedef struct node* Position;
+struct node {
+	int koef;
+	int eksp;
+	Position next;
 };
-
-int inputF(Position, char[], char[], int);
-void printList(Position);
-int inputE(Position, char[], char[], int);
-Position findEl(Position, char[]);
-Position findPrev(Position,char[]);
-int deletEl(Position, char[]);
-int PushBefore(Position, char[]);
-int PushAfter(Position, char[]);
-int PushSorted(Position);
-int WriteInFile(Position);
-int ReadFromFile(Position);
-int deleteAll(Position);
-
+int readPolynom(Position);
+int sumaPolynom(Position, Position, Position);
+int MulPolynom(Position, Position, Position);
+int printList(Position);
+int deletAll(Position);
 int main() {
-
-	Osoba head;
-	head.Next = NULL;
-	Position person = NULL;
-	int choice = 0;
-	int check = 0;
-	char firstname[NAME_LEN];
-	char lastname[NAME_LEN];
-	int birthYear;
-	char target[NAME_LEN] = { 0 };
-
-	
-
-	while (choice != 12) {
-
-		printf("Izbornik:\n");
-		printf("1. Dodaj element na pocetak\n");
-		printf("2. Ispisi listu\n");
-		printf("3. Dodaj element na kraj\n");
-		printf("4. Pronadi element po prezimenu\n");
-		printf("5. Izbrisi element po prezimenu\n");
-		printf("6. Dodaj element iza odredenog elementa\n");
-		printf("7. Dodaj element ispred odredenog elementa\n");
-		printf("8. Sortirano dodaj\n");
-		printf("9. Upisi listu u datoteku\n");
-		printf("10. Citaj listu iz datoteke\n");
-		printf("11. Izbrisi cijelu listu\n");
-		printf("12. Izlaz\n");
-		printf("#########################################################\n");
-		printf("Unesite izbor\n");
-		scanf(" %d", &choice);
-
-		switch (choice) {
-		case 1:
-			printf("Unesite ime, prezime i godinu rodenja\n");
-			scanf(" %s %s %d", firstname, lastname, &birthYear);
-			check=inputF(&head, firstname, lastname, birthYear);
-			if (check == -1) {
-				printf(" ERROR!! Alokacije nije izvrsena\n");
-			}
-			else {
-				printf(" Sve je proslo u redu.\n");
-			}
-			break;
-		case 2:
-			printList(head.Next);
-			break;
-		case 3:
-			printf("Unesite ime, prezime i godinu rodenja\n");
-			scanf(" %s %s %d", firstname, lastname, &birthYear);
-			check=inputE(&head, firstname, lastname, birthYear);
-			if (check == -1) {
-				printf(" ERROR!! Alokacije nije izvrsena\n");
-			}
-			else {
-				printf(" Sve je proslo u redu.\n");
-			}
-			break;
-		case 4:
-			printf("Upisite trazeno prezime:\n");
-			scanf(" %s", target);
-			person = findEl(head.Next,target);
-			if (person != NULL) {
-				printf("Trazeni osoba je:\n %s %s %d\n", person->firstname, person->lastname, person->birthYear);
-			}
-			else
-				printf("Nije pronadena osoba s tim prezimenom\n");
-			break;
-		case 5:
-			printf("Upisite trazeno prezime:\n");
-			scanf(" %s", target);
-			deletEl(&head,target);
-			printList(head.Next);
-			break;
-		case 6:
-			printf("Upisite trazeno prezime:\n");
-			scanf(" %s", target);
-			check = PushAfter(&head, target);
-			if (check == -1) {
-				printf(" ERROR!! Alokacije nije izvrsena\n");
-			}
-			else {
-				printf(" Sve je proslo u redu.\n");
-			}
-			break;
-		case 7:
-			printf("Upisite trazeno prezime:\n");
-			scanf(" %s", target);
-			check = PushBefore(&head, target);
-			if (check == -1) {
-				printf(" ERROR!! Alokacije nije izvrsena\n");
-			}
-			else {
-				printf(" Sve je proslo u redu.\n");
-			}
-			break;
-		case 8:
-			check = PushSorted(&head);
-			if (check == -1) {
-				printf(" ERROR!! Alokacije nije izvrsena\n");
-			}
-			else {
-				printf(" Sve je proslo u redu.\n");
-			}
-			break;
-		case 9:
-			check = WriteInFile(&head);
-			if (check == -1) {
-				printf(" ERROR!! Alokacije nije izvrsena\n");
-			}
-			else {
-				printf(" Sve je proslo u redu.\n");
-			}
-			break;
-		case 10:
-			check = ReadFromFile(&head);
-			if (check == -1) {
-				printf(" ERROR!! Alokacije nije izvrsena\n");
-			}
-			else {
-				printf(" Sve je proslo u redu.\n");
-				printList(head.Next);
-			}
-			break;
-		case 11:
-			check = deleteAll(&head);
-			if (check == -1) {
-				printf("ERROR!\n");
-				return -1;
-			}
-			break;
-		case 12:
-			printf("Kraj odabira!\n");
-			break;
-		default:
-			printf("Pogresan izbor\nPokusajte ponovo");
-			break;
-		}
+	struct node head1;
+	struct node head2;
+	struct node headSuma;
+	struct node headMultiple;
+	int result = 0;
+	head1.next = NULL;
+	head2.next = NULL;
+	headSuma.next = NULL;
+	headMultiple.next = NULL;
+	result = readPolynom(&head1);
+	if (result == 1) {
+		printf("Datoteka nije otvorena!\n");
+		return 0;
 	}
-
-	return 0;
-}
-
-int inputF(Position P, char firstname[],char lastname[],int birthYear) {
-
-	Position q = (Position*)malloc(sizeof(struct osoba));
-	if (q == NULL) {
-		printf("Alokacija nije uspijela\n");
-		return -1;
-	}
-	strcpy(q->firstname, firstname);
-	strcpy(q->lastname, lastname);
-	q->birthYear = birthYear;
-	q->Next = P->Next;
-	P->Next = q;
-
-	return 0;
-}
-
-void printList(Position P) {
-	if (P == NULL) {
-		printf("Lista je prazna\n");
+	else if (result == -1) {
+		printf("Greska pri alokaciji memorije!\n");
+		deletAll(&head1);
+		return 0;
 	}
 	else {
-		printf("Ispis liste:\n");
-		while (P != NULL) {
-			printf("%s %s %d\n", P->firstname, P->lastname, P->birthYear);
-			P=P->Next;
-		}
+		printf("Sve je proslo u redu!\n");
 	}
-}
-
-int inputE(Position P, char firstname[], char lastname[], int birthYear) {
-	while (P->Next != NULL)
-		P = P->Next;
-
-	return inputF(P, firstname, lastname, birthYear);
-}
-Position findEl(Position P, char lastname[]) {
-	while (P != NULL && strcmp(P->lastname, lastname) != 0)
-		P = P->Next;
-
-	return P;
-}
-Position findPrev(Position P, char lastname[]) {
-	while (P->Next != NULL && strcmp(P->Next->lastname, lastname) != 0) {
-		P = P->Next;
+	result = readPolynom(&head2);
+	if (result == 1) {
+		printf("Datoteka nije otvorena!\n");
+		return 0;
 	}
-
-	if (P->Next != NULL) {
-		return P;
+	else if (result == -1) {
+		printf("Greska pri alokaciji memorije!\n");
+		deletAll(&head2);
+		return 0;
 	}
-	return NULL;
-}
-int deletEl(Position P, char lastname[]) {
-	Position temp = NULL;
-
-	P = findPrev(P,lastname);
-
-	if (P != NULL) {// za cijelu listu je potrebna while petlja da sve izbrise alii istog principa
-		temp = P->Next;
-		P->Next = temp->Next;
-		printf("Brisemo osobu: %s %s %d\n", temp->firstname, temp->lastname, temp->birthYear);
-		free(temp);
+	else {
+		printf("Sve je proslo u redu!\n");
 	}
+	printf("Prvi polinom:\t");
+	printList(head1.next);
+	printf("Drugi polinom:\t");
+	printList(head2.next);
+	result = sumaPolynom(head1.next, head2.next, &headSuma);
+	if (result == -1) {
+		printf("Greska pri alokaciji memorije!\n");
+		deletAll(&head1);
+		deletAll(&head2);
+		deletAll(&headSuma);
+		return 0;
+	}
+	else {
+		printf("Sve je proslo u redu!\n");
+	}
+	printf("Suma polinoma:\t");
+	printList(headSuma.next);
+	result = MulPolynom(head1.next, head2.next, &headMultiple);
+	if (result == -1) {
+		printf("Greska pri alokaciji memorije!\n");
+		deletAll(&head1);
+		deletAll(&head2);
+		deletAll(&headMultiple);
+		return 0;
+	}
+	else {
+		printf("Sve je proslo u redu!\n");
+	}
+	printf("Produkt polinoma:");
+	printList(headMultiple.next);
+
+	deletAll(&head1);
+	deletAll(&head2);
+	deletAll(&headSuma);
+	deletAll(&headMultiple);
+	printList(headMultiple.next);
+	printList(headSuma.next);
+	printList(head1.next);
+	printList(head2.next);
 	return 0;
 }
-int PushAfter(Position P, char target[]) {
-	char firstname[NAME_LEN];
-	char lastname[NAME_LEN];
-	int birthYear;
+int readPolynom(Position P) {
+	FILE* fp = NULL;
+	char fileName[128] = "\0";
+	Position q = NULL;
 
-	P = findEl(P, target);
-
-	if (P == NULL) {
-		return -1;
-	}
-
-	printf("Unesite ime, prezime i godinu rodenja\n");
-	scanf(" %s %s %d", firstname, lastname, &birthYear);
-
-	return inputF(P, firstname, lastname, birthYear);
-}
-int PushBefore(Position P, char target[]) {
-	char firstname[NAME_LEN];
-	char lastname[NAME_LEN];
-	int birthYear; 
-	P = findPrev(P, target);
-	if (P == NULL) {
-		return -1;
-	}
-	printf("Unesite ime, prezime i godinu rodenja\n");
-	scanf(" %s %s %d", firstname, lastname, &birthYear);
-	return inputF(P, firstname, lastname, birthYear);
-}
-int PushSorted(Position P) {
-	Position q = (Position)malloc(sizeof(struct osoba));
-	if (q == NULL) {
-		printf("Alokacija nije uspjela!\n");
-		return -1;
-	}
-	printf("Unesite ime, prezime i godinu rodenja\n");
-	scanf(" %s %s %d", q->firstname, q->lastname, &q->birthYear);
-	while (P->Next != NULL && _strcmpi(P->Next->lastname, q->lastname) < 0) {
-		P = P->Next;
-	}
-	q->Next = P->Next;
-	P->Next = q;
-	return 0;
-}
-int ReadFromFile(Position P) {
-	int result = 0;
-	char firstname[NAME_LEN];
-	char lastname[NAME_LEN];
-	int birthYear = 0;
-	char filename[NAME_LEN];
-	printf("Upisite ime datoteke\n");
-	scanf(" %s", &filename);
-	FILE* fp = fopen(filename, "r");
+	printf("Upisi ime datoteke:\n");
+	scanf(" %s", fileName);
+	fp = fopen(fileName, "r");
 	if (fp == NULL) {
-		printf("Datoteka nije pronadena!\n");
+		printf("Datoteka nije otvorena!\n");
 		return 1;
 	}
-	while ((fscanf(fp, " %s %s %d", firstname, lastname, &birthYear) > 0)) {
-		result = inputF(P, firstname, lastname, birthYear);
-		if (result != 0) {
+	int koef, eksp;
+	while (fscanf(fp, "%d %d", &koef, &eksp) == 2) {
+		q = (Position)malloc(sizeof(struct node));
+		if (q == NULL) {
+			printf("Greška pri alokaciji memorije!\n");
 			fclose(fp);
-			return result;
+			return -1;
+		}
+
+		// Postavljanje vrednosti 
+		q->koef = koef;
+		q->eksp = eksp;
+
+		// Umetanje elementa u sortiranu listu
+		while (P->next != NULL && P->next->eksp > q->eksp) {
+			P = P->next;
+		}
+
+		q->next = P->next;
+		P->next = q;
+	}
+	fclose(fp);
+	return 0;
+}
+int sumaPolynom(Position P1, Position P2, Position Suma) {
+	Position q = NULL;
+	while (P1 != NULL || P2 != NULL)
+	{
+		q = (Position)malloc(sizeof(struct node));
+		if (q == NULL) {
+			printf("Greska pri alokaciji memorije!\n");
+			return -1;
+		}
+
+		if (P1 == NULL) {  // Ako je prvi polinom prazan, preuzmi element iz drugog polinoma
+			q->eksp = P2->eksp;
+			q->koef = P2->koef;
+			P2 = P2->next;
+		}
+		else if (P2 == NULL) {
+			q->eksp = P1->eksp;
+			q->koef = P1->koef;
+			P1 = P1->next;
+		}
+		else if (P1->eksp > P2->eksp) {   // Ako je eksponent iz prvog polinoma veæi, dodaj taj element u sumu
+			q->eksp = P1->eksp;
+			q->koef = P1->koef;
+			P1 = P1->next;
+		}
+		else if (P1->eksp < P2->eksp) {
+			q->eksp = P2->eksp;
+			q->koef = P2->koef;
+			P2 = P2->next;
+		}
+		else {   // Ako su eksponenti jednaki, zbroji koeficijente
+			q->eksp = P1->eksp;
+			q->koef = P1->koef + P2->koef;
+			P1 = P1->next;
+			P2 = P2->next;
+		}
+
+		q->next = Suma->next;
+		Suma->next = q;
+		Suma = q;  // Pokazivaè Suma  pomakni na kraj nove liste
+
+	}
+
+	return 0;
+}
+int MulPolynom(Position P1, Position P2, Position multiple) {
+	Position q = NULL, i = NULL, j = NULL, tmp = NULL;
+	int eksp = 0;
+	int koef = 0;
+	
+	for (i = P1; i != NULL; i = i->next) {
+		for (j = P2; j != NULL; j = j->next) {
+			eksp = i->eksp + j->eksp;
+			koef = i->koef * j->koef;
+
+			tmp = multiple;
+			while (tmp->next != NULL && tmp->next->eksp > eksp) {
+				tmp = tmp->next;
+			}
+			// Ako je eksponent veæ prisutan u rezultatu, samo zbrojimo koeficijente
+			if (tmp->next != NULL && tmp->next->eksp == eksp) {
+				tmp->next->koef += koef;
+			}
+			else {
+				// Ako eksponent nije prisutan, stvaramo novi èvor
+				q = (Position)malloc(sizeof(struct node));
+				if (q == NULL) {
+					printf("Greska pri alokaciji memorije!\n");
+					return -1;
+				}
+				q->eksp = eksp;
+				q->koef = koef;
+				q->next = tmp->next;
+				tmp->next = q;
+			}
 		}
 	}
-
-	fclose(fp);
 	return 0;
 }
-
-int WriteInFile(Position P) {
-	char filename[NAME_LEN] = { '\0' };
-	printf("Unesite ime datoteke\n");
-	scanf(" %s", &filename);
-	FILE* fp = fopen(filename, "w");
-	if (fp == NULL) {
-		printf("Alokacija nije uspjela!\n");
-		return -1;
+int printList(Position P) {
+	if (P == NULL) {
+		printf("Prazna lista\n");
+		return 0;
 	}
-	while (P->Next != NULL) {
-		fprintf(fp, " %s %s %d\n", P->Next->firstname, P->Next->lastname, P->Next->birthYear);
-		P->Next = P->Next->Next;
+	while (P != NULL) {
+		
+		if (P->koef > 0) {
+			printf("+%dx^%d ", P->koef, P->eksp);
+		}
+		else {
+			printf("%dx^%d ", P->koef, P->eksp);
+		}
+		P = P->next;
 	}
-	/*while (P != NULL) {
-		fprintf(fp, " %s %s %d\n", P->firstname, P->lastname, P->birthYear);
-		P = P->Next;
-	}*/
-	fclose(fp);
+	printf("\n");
 	return 0;
 }
-int deleteAll(Position P) {
-	Position temp=NULL;
-	while (P->Next != NULL) {
-		temp = P->Next;
-		P->Next = temp->Next;
+int deletAll(Position P) {
+	Position temp = NULL;
+
+	while (P->next != NULL) {
+		temp = P->next;
+		P->next = temp->next;
 		free(temp);
 	}
+
 	return 0;
 }
